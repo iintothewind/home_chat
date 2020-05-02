aeed855cae17b4e88f25932bd5a0b9c99cb49db9
Ivar <ivarchen@live.cn>
2020-05-02 15:30:33 +0800

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
 import { MessageBoard } from './modules/MessageBoard'
 import { BrowserRouter, Route } from 'react-router-dom'
-/*
-ReactDOM.render(<Input rows={4} defaultValue="this is a text area" />, document.getElementById('txtArea'));
-
-const Msg = () => {
-  return (
-    <Comment
-      author={"Han Solo"}
-      content={
-        <p>
-          We supply a series of design principles, practical patterns and high quality design
-          resources (Sketch and Axure), to help people create their product prototypes beautifully
-          and efficiently.
-        </p>
-      }
-      datetime={
-        <Tooltip title={moment().format('YYYY-MM-DD HH:mm:ss')}>
-          <span>{moment().fromNow()}</span>
-        </Tooltip>
-      }
-    />
-  );
-}
-
-ReactDOM.render(<Msg />, document.getElementById('msg'));
-*/
-
-// ReactDOM.render(<MessageBoard />, document.getElementById('app'));
 
 ReactDOM.render((
   <BrowserRouter>
