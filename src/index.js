8f25ba399e9dcfb535efe8a6d417ef558a674b67
yinghchen <yinghchen@paypal.com>
2020-06-02 12:57:40 +0800

    move app to snd domain name for nginx

--- src/index.js
+++ src/index.js
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom';
 import { MessageBoard } from './modules/MessageBoard'
 import { BrowserRouter, Route } from 'react-router-dom'
 
+const cfg = require('../package.json')
+
 ReactDOM.render((
-  <BrowserRouter>
+  <BrowserRouter basename={cfg.name}>
     <Route path="/messageBoard" component={MessageBoard}/>
   </BrowserRouter>
 ), document.getElementById('app'))
