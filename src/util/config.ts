8aa998d164c07ff31f8557580450973b4f2e0e13
iintothewind <ivarchen@live.cn>
2020-06-28 00:20:13 +0800

    added mqtts url

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,6 +2,7 @@ export const cfg = {
   appKey: 'home_chat',
   dbVersion: 1,
   mqttUrl: 'ws://192.168.0.147:1884',
+  mqttsUrl: 'wss://mqtt.eclipse.org:443/mqtt',
   mqttDefaultTopic: 'home_chat/general',
   maxInputLength: 5120,
 }
