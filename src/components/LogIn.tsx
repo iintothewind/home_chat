0469934c6567b87a14997761ddb76f899571b65b
iintothewind <ivarchen@live.cn>
2020-07-05 22:45:01 +0800

    added login

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -1,7 +1,23 @@
 import React from 'react'
-import { List, Comment, Layout, notification, Tooltip } from 'antd'
+import { Layout, notification, message, Tooltip } from 'antd'
+import { GithubOutlined } from '@ant-design/icons'
+import '../styles/login.css'
 
 const { Footer, Content } = Layout
 
 
+const Login = () => {
 
+  return (
+    <Layout className='login-layout'>
+      <Content className='login-content'>
+        <GithubOutlined className='login-github' />
+      </Content>
+      <Footer>
+        <GithubOutlined style={{ fontSize: '48px', color: '#08c' }} />
+      </Footer>
+    </Layout>
+  )
+}
+
+export { Login }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Result, Button } from 'antd'
 import MessageList from './components/MessageList'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { Login } from './components/login'
+import { BrowserRouter, Route, Switch, useHistory, useLocation } from 'react-router-dom'
 import { cfg } from './util/config'
 
+interface LocationState {
+  from: {
+    pathname: string
+  }
+}
+
+const NotFound = () => {
+  const history = useHistory()
+  const location = useLocation<LocationState>()
+  const { from } = location.state || { from: { pathname: '/' } }
+
+  return <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={<Button type="primary" onClick={() => history.replace(from)}>Back Home</Button>} />
+}
 
 ReactDOM.render((
-  <BrowserRouter basename={cfg.appKey}>
-    <Route path="/messageList" component={MessageList} />
+  <BrowserRouter basename={cfg.baseName}>
+    <Switch>
+      <Route exact path="/" component={Login} />
+      <Route exact path="/messageList" component={MessageList} />
+      <Route path="*" component={NotFound} />
+    </Switch>
   </BrowserRouter>
 ), document.getElementById('app'))
