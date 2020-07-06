37bd7bc260426b650f9759484688c07bdc127599
iintothewind <ivarchen@live.cn>
2020-07-06 09:45:14 +0800

    updated login css

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Layout, notification, message, Tooltip } from 'antd'
 import { GithubOutlined, LoginOutlined } from '@ant-design/icons'
-import { useHistory, useLocation } from 'react-router-dom'
 import '../styles/login.css'
 
 const { Footer, Content } = Layout
@@ -11,12 +10,14 @@ const Login = () => {
   return (
     <Layout className='login-layout'>
       <Content className='login-content'>
-        <Tooltip title='click to sign in with github'>
-          <a href='https://github.com/login/oauth/authorize?client_id=b60569827b30c0e7e58e&scope=user:emai'><GithubOutlined className='login-github' /></a>
-        </Tooltip>
+        <a href='https://github.com/login/oauth/authorize?client_id=b60569827b30c0e7e58e&scope=user:emai'>
+          <Tooltip title='click to sign in with github' placement='top'>
+            <GithubOutlined className='login-github' />
+          </Tooltip>
+        </a>
       </Content>
       <Footer className='login-footer'>
-        <Tooltip title='click to login in as guest'>
+        <Tooltip title='click to login in as guest' placement='bottom'>
           <LoginOutlined className='login-guest' />
         </Tooltip>
       </Footer>
