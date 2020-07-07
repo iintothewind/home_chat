4daca70b096cc30adee363bd0bbdbeef995e6861
yinghchen <yinghchen@paypal.com>
2020-07-07 21:44:38 +0800

    updated login

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import fetch from 'node-fetch'
 import { Layout, Tooltip } from 'antd'
 import { GithubOutlined, LoginOutlined } from '@ant-design/icons'
-import { useHistory, useLocation } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import '../styles/login.css'
 
 const { Footer, Content } = Layout
@@ -14,26 +15,62 @@ interface LoginState {
   code?: string
 }
 
+interface AccessToken {
+  token: string
+}
+
+interface UserInfo {
+  login: string
+  name: string
+  avatarUrl: string
+}
+
+const fetchUser = async (code: string) => {
+  const ac: AccessToken = await fetch(`https://gatekeeper-iintothewind.herokuapp.com/authenticate/${code}`)
+    .then(resp => resp.json())
+  console.log(`ac: ${ac.token}`)
+  const user: UserInfo = await fetch(`https://api.github.com/user`, { headers: [['Authorization', `Bearer ${ac.token}`], ['Access-Control-Allow-Origin', '*']] })
+    .then(resp => resp.json())
+  console.log(`user: ${user}`)
+  return user
+}
+
 
 const Login = () => {
   const query = useQuery()
+  const code = query.get('code')
+  const [state, setState] = useState<UserInfo | null>(null)
+
+  useEffect(() => {
+    if (code) {
+      console.log(`code: ${code}`)
+      fetchUser(code)
+        .then(user => setState(user))
+        .catch(error => console.log(error))
+    }
+  })
 
 
   return (
-    <Layout className='login-layout'>
-      <Content className='login-content'>
-        <a href='https://github.com/login/oauth/authorize?client_id=b60569827b30c0e7e58e&scope=user:emai'>
-          <Tooltip title='click to sign in with github' placement='top'>
-            <GithubOutlined className='login-github' />
-          </Tooltip>
-        </a>
-      </Content>
-      <Footer className='login-footer'>
-        <Tooltip title='click to login in as guest' placement='bottom'>
-          <LoginOutlined className='login-guest' />
-        </Tooltip>
-      </Footer>
-    </Layout>
+    code ?
+      <p>code: {code}, user: {state}</p>
+      :
+      <>
+        <Layout className='login-layout'>
+          <Content className='login-content'>
+            <a href='https://github.com/login/oauth/authorize?client_id=b60569827b30c0e7e58e&scope=user:emai'>
+              <Tooltip title='click to sign in with github' placement='top'>
+                <GithubOutlined className='login-github' />
+              </Tooltip>
+            </a>
+          </Content>
+          <Footer className='login-footer'>
+            <Tooltip title='click to login in as guest' placement='bottom'>
+              <LoginOutlined className='login-guest' />
+            </Tooltip>
+          </Footer>
+        </Layout>
+      </>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import MessageList from './components/MessageList'
 import { Login } from './components/login'
 import { BrowserRouter, Route, Switch, useHistory, useLocation } from 'react-router-dom'
 import { cfg } from './util/config'
+import { useEffect, useState } from 'react'
 
 interface LocationState {
   from: {
@@ -18,17 +19,37 @@ const NotFound = () => {
   const { from } = location.state || { from: { pathname: '/' } }
 
   return <Result
-    status="404"
-    title="404"
-    subTitle="Sorry, the page you visited does not exist."
-    extra={<Button type="primary" onClick={() => history.replace(from)}>Back Home</Button>} />
+    status='404'
+    title='NotFound'
+    subTitle='Sorry, the page you visited does not exist.'
+    extra={<Button type='primary' onClick={() => history.replace(from)}>Back Home</Button>} />
+}
+
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  });
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
 }
 
 ReactDOM.render((
   <BrowserRouter basename={cfg.baseName}>
     <Switch>
-      <Route exact path="/" component={Login} />
-      <Route exact path="/messageList" component={MessageList} />
+      <Route exact path='/' component={Login} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/messageList' component={MessageList} />
+      <Route exact path='/test' component={Example} />
       <Route path="*" component={NotFound} />
     </Switch>
   </BrowserRouter>
