e5b877400eeddca81d5789bf74d6f61d877a34e3
yinghchen <yinghchen@paypal.com>
2020-07-09 20:50:11 +0800

    removed unncesary code

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,6 @@ import MessageList from './components/MessageList'
 import { Login } from './components/login'
 import { BrowserRouter, Route, Switch, useHistory, useLocation } from 'react-router-dom'
 import { cfg } from './util/config'
-import { useEffect, useState } from 'react'
 
 interface LocationState {
   from: {
@@ -25,31 +24,12 @@ const NotFound = () => {
     extra={<Button type='primary' onClick={() => history.replace(from)}>Back Home</Button>} />
 }
 
-
-function Example() {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  });
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>
-        Click me
-      </button>
-    </div>
-  );
-}
-
 ReactDOM.render((
   <BrowserRouter basename={cfg.baseName}>
     <Switch>
       <Route exact path='/' component={Login} />
       <Route exact path='/login' component={Login} />
       <Route exact path='/messageList' component={MessageList} />
-      <Route exact path='/test' component={Example} />
       <Route path="*" component={NotFound} />
     </Switch>
   </BrowserRouter>
