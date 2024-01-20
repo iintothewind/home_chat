da22ba7b3715cb9956d497d46ea8fe3f0d3a97fd
iintothewind <ivarchen@live.cn>
2024-01-19 22:15:04 -0800

    updated with mqttsUrl

--- src/util/config.ts
+++ src/util/config.ts
@@ -10,8 +10,8 @@ export const cfg = {
   //mqttsUrl: 'wss://mqtt.eclipseprojects.io:443/mqtt',
   // initMqttConnection looks up window location protocol to check with mqtt or mqtts should be used
   mqttUrl: 'ws:///121.5.128.159:8083/mqtt',
-  // mqttsUrl: 'wss://121.5.128.159:8084/mqtt',
-  mqttsUrl: 'wss://mqtt.eclipseprojects.io:443/mqtt',
+  mqttsUrl: 'ws://23.239.7.88:8083/mqtt',
+  // mqttsUrl: 'wss://mqtt.eclipseprojects.io:443/mqtt',
   mqttTopicPrefx: 'home_chat/',
   mqttDefaultTopic: 'general',
   backendUrl: 'https://121.5.128.159:8443',
